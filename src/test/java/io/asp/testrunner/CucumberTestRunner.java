262f36f6f6fb07836b0db8b3901ab36a054d722d
anjuk <anjuk@192.168.0.54>
2023-07-08 19:50:12 +0530

    Updated TestRunner and testNG.xml file

--- src/test/java/io/asp/testrunner/CucumberTestRunner.java
+++ src/test/java/io/asp/testrunner/CucumberTestRunner.java
@@ -4,10 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = {"classpath:featureFiles/googleSearch.feature"},
-		glue = { "stepdefinitions" }, 
-		plugin = { "pretty", "html:target/cucumber-reports/cucumberreport.html" },
-		monochrome = true)
+		features = {"classpath:featureFiles"}, // Path of the feature files
+		glue = { "io/asp/stepdefinitions" }, // Path of the step definitions
+		plugin = { "pretty", "html:target/cucumber-reports/cucumberreport.html" }, // To generate different format of test reports
+		monochrome = true, // Display the console output in a proper readable format 
+		dryRun =false) // to check if the mapping b/w feature file and step definitions file
 
 public class CucumberTestRunner extends AbstractTestNGCucumberTests{
 
